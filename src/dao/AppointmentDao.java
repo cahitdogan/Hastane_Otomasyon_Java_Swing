df6569b11f0a7b6181694902fc714dfb777279ed
Cahit <cahitdogan999@gmail.com>
2025-01-24 15:07:46 +0300

    fixed bad architect of createApoointment, added getAppointmentsByPatient

--- src/dao/AppointmentDao.java
+++ src/dao/AppointmentDao.java
@@ -5,7 +5,9 @@ import entity.Appointment;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class AppointmentDao {
     Connection connection;
@@ -14,37 +16,57 @@ public class AppointmentDao {
         this.connection = Database.getInstance();
     }
 
-    public boolean createAppointment(Appointment appointment, int patientID, int appointmentID, int doctorID) {
+    public boolean createAppointment(Appointment appointment) {
         PreparedStatement pr = null;
-        String query = "";
         try {
-            query = "INSERT INTO appointments (patient-name, doctor-name, patient-tc, date, time) VALUES (?,?,?,?,?)";
+            String query = "INSERT INTO appointments (patient_name, doctor_name, patient_tc, month, day, hour, branch, status, doctor_id, patient_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             pr = connection.prepareStatement(query);
             pr.setString(1, appointment.getPatientName());
             pr.setString(2, appointment.getDoctorName());
-            pr.setString(3, appointment.getBranch());
-            pr.setString(4, appointment.getPatientTc());
-            pr.setString(5, appointment.getMonth());
-            pr.setInt(6, appointment.getDay());
-            pr.setString(7, appointment.getHour());
+            pr.setString(3, appointment.getPatientTc());
+            pr.setString(4, appointment.getMonth());
+            pr.setInt(5, appointment.getDay());
+            pr.setString(6, appointment.getHour());
+            pr.setString(7, appointment.getBranch());
+            pr.setString(8, appointment.getStatus());
+            pr.setInt(9, appointment.getDoctorId());
+            pr.setInt(10, appointment.getPatientId());
             int affectedRows = pr.executeUpdate();
-
-
-            query = "INSERT INTO PatientAppointment VALUES (?, ?)";
-            pr = connection.prepareStatement(query);
-            pr.setInt(1, patientID);
-            pr.setInt(2, appointmentID);
-            pr.executeUpdate();
-
-            query = "INSERT INTO DoctorAppointment VALUES (?, ?)";
-            pr = connection.prepareStatement(query);
-            pr.setInt(1, doctorID);
-            pr.setInt(2, appointmentID);
-            pr.executeUpdate();
-
             return affectedRows > 0;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public ArrayList<Appointment> getAppointmentsByPatient(int patientId) {
+        String query = "SELECT * FROM appointments WHERE patient_id = ?";
+        try {
+            PreparedStatement pr = this.connection.prepareStatement(query);
+            pr.setInt(1, patientId);
+            ResultSet rs = pr.executeQuery();
+            ArrayList<Appointment> appointments = new ArrayList<>();
+            while (rs.next()) {
+                appointments.add(match(rs));
+            }
+            return appointments;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    private static Appointment match(ResultSet rs) throws SQLException {
+        Appointment appointment = new Appointment();
+        appointment.setDoctorId(rs.getInt("id"));
+        appointment.setStatus(rs.getString("status"));
+        appointment.setPatientId(rs.getInt("patient_id"));
+        appointment.setDoctorName(rs.getString("doctor_name"));
+        appointment.setPatientName(rs.getString("patient_name"));
+        appointment.setPatientTc(rs.getString("patient_tc"));
+        appointment.setMonth(rs.getString("month"));
+        appointment.setDay(rs.getInt("day"));
+        appointment.setHour(rs.getString("hour"));
+        appointment.setBranch(rs.getString("branch"));
+
+        return appointment;
     }
 }
