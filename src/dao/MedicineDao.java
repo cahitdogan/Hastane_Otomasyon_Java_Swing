97ae98275f4e6cf9c5e3da2877f9ad6cb19e2546
Cahit <cahitdogan999@gmail.com>
2025-01-24 15:11:04 +0300

    added getMedicinesByPatient(), match()

--- src/dao/MedicineDao.java
+++ src/dao/MedicineDao.java
@@ -1,8 +1,14 @@
 package dao;
 
 import core.Database;
+import entity.Medicine;
+import entity.Patient;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MedicineDao {
     private Connection connection;
@@ -11,5 +17,29 @@ public class MedicineDao {
         this.connection = Database.getInstance();
     }
 
+    public ArrayList<Medicine> getMedicinesByPatient(int patientId) {
+        String query = "SELECT medicines.id, medicines.name, medicines.dosage, medicines.stock FROM medicines INNER JOIN PatientMedicine ON PatientMedicine.MedicineID = medicines.id WHERE PatientMedicine.patientID = ?";
+        try {
+            PreparedStatement pr = this.connection.prepareStatement(query);
+            pr.setInt(1, patientId);
+            ResultSet rs = pr.executeQuery();
+            ArrayList<Medicine> medicines = new ArrayList<>();
+            while (rs.next()) {
+                medicines.add(match(rs));
+            }
+            return medicines;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
+    }
+
+    private static Medicine match(ResultSet rs) throws SQLException {
+        Medicine medicine = new Medicine();
+        medicine.setId(rs.getInt("id"));
+        medicine.setDosage(rs.getInt("dosage"));
+        medicine.setName(rs.getString("name"));
+        medicine.setStock(rs.getInt("stock"));
+        return medicine;
+    };
 }
